66eb475c3032aa788e48f013362348ac32b0c47f
MepisashviliGuga <gugamepisashvili2@gmail.com>
2026-03-23 19:44:35 +0400

    feat: add pipeline behaviors for validation and logging

---
+++ SmartAssistHub.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = SmartAssistHub.Application.Common.Exceptions.ValidationException;
+
+namespace SmartAssistHub.Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(
+        IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next();
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(
+            _validators.Select(v =>
+                v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .GroupBy(
+                f => f.PropertyName,
+                f => f.ErrorMessage,
+                (property, errors) => new
+                {
+                    Key = property,
+                    Values = errors.Distinct().ToArray()
+                })
+            .ToDictionary(x => x.Key, x => x.Values);
+
+        if (failures.Any())
+            throw new ValidationException(failures);
+
+        return await next();
+    }
+}
--- SmartAssistHub.Application/DependencyInjection.cs
+++ SmartAssistHub.Application/DependencyInjection.cs
@@ -1,21 +1,29 @@
-﻿using System.Reflection;
-using FluentValidation;
+﻿using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using SmartAssistHub.Application.Common.Behaviors;
+using System.Reflection;
 
 namespace SmartAssistHub.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(
-        this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(
-                Assembly.GetExecutingAssembly()));
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
 
-        services.AddValidatorsFromAssembly(
-            Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(
+                typeof(IPipelineBehavior<,>),
+                typeof(LoggingBehavior<,>));
+
+            cfg.AddBehavior(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
