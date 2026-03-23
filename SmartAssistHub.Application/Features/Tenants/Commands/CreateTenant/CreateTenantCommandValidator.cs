0557bbe1d8e94a5c4d1a3f46b5cbdfb5b22dc52f
MepisashviliGuga <gugamepisashvili2@gmail.com>
2026-03-23 19:59:16 +0400

    feat: add CreateTenant command, validator and handler

---
+++ SmartAssistHub.Application/Features/Tenants/Commands/CreateTenant/CreateTenantCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace SmartAssistHub.Application.Features.Tenants.Commands.CreateTenant;
+
+public class CreateTenantCommandValidator
+    : AbstractValidator<CreateTenantCommand>
+{
+    public CreateTenantCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Tenant name is required.")
+            .MaximumLength(100)
+            .WithMessage("Tenant name cannot exceed 100 characters.")
+            .MinimumLength(2)
+            .WithMessage("Tenant name must be at least 2 characters.");
+
+        RuleFor(x => x.Plan)
+            .IsInEnum()
+            .WithMessage("Invalid tenant plan specified.");
+    }
+}
