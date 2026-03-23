27a6d1345d049929043cd59e582f990b371624b7
MepisashviliGuga <gugamepisashvili2@gmail.com>
2026-03-23 17:54:40 +0400

    feat: add unit of work interface and dependency injection setup

---
+++ SmartAssistHub.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartAssistHub.Application.Common.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default);
+}
