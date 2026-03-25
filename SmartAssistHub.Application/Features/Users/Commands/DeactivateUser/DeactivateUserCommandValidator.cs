83c917abf619a90de8b52bdb793b57577caf0a12
MepisashviliGuga <gugamepisashvili2@gmail.com>
2026-03-26 01:08:50 +0400

    feat: add DeactivateUser, ChangeUserRole and TransferOwnership commands

---
+++ SmartAssistHub.Application/Features/Users/Commands/DeactivateUser/DeactivateUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace SmartAssistHub.Application.Features.Users.Commands.DeactivateUser;
+
+public class DeactivateUserCommandValidator : AbstractValidator<DeactivateUserCommand>
+{
+    public DeactivateUserCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .WithMessage("UserId is required.");
+
+        RuleFor(x => x.TenantId)
+            .NotEmpty()
+            .WithMessage("TenantId is required.");
+    }
+}
+
---
+++ SmartAssistHub.Application/Features/Users/Commands/TransferOwnership/TransferOwnershipCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using SmartAssistHub.Application.Common.Exceptions;
+using SmartAssistHub.Application.Common.Interfaces;
+using SmartAssistHub.Application.Common.Interfaces.Repositories;
+
+namespace SmartAssistHub.Application.Features.Users.Commands.TransferOwnership;
+
+public class TransferOwnershipCommandHandler
+    : IRequestHandler<TransferOwnershipCommand, Unit>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public TransferOwnershipCommandHandler(
+        IUserRepository userRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(
+        TransferOwnershipCommand command,
+        CancellationToken cancellationToken)
+    {
+        var currentOwner = await _userRepository
+            .GetByIdAsync(command.CurrentOwnerId, cancellationToken);
+
+        if (currentOwner is null)
+            throw new NotFoundException(
+                nameof(currentOwner), command.CurrentOwnerId);
+
+        if (currentOwner.TenantId != command.TenantId)
+            throw new ForbiddenException(
+                "Current owner does not belong to this tenant.");
+
+        var newOwner = await _userRepository
+            .GetByIdAsync(command.NewOwnerId, cancellationToken);
+
+        if (newOwner is null)
+            throw new NotFoundException(
+                nameof(newOwner), command.NewOwnerId);
+
+        if (newOwner.TenantId != command.TenantId)
+            throw new ForbiddenException(
+                "New owner does not belong to this tenant.");
+
+        currentOwner.TransferOwnership(newOwner);
+
+        await _userRepository.UpdateAsync(currentOwner, cancellationToken);
+        await _userRepository.UpdateAsync(newOwner, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
